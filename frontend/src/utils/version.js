7c7e10b23aa90b2ac82a41db4b6b29b688bb8075
GitHub Action <action@github.com>
2025-03-02 00:08:07 +0000

    Update version to v1.0.1

--- frontend/src/utils/version.js
+++ frontend/src/utils/version.js
@@ -1,4 +1,4 @@
 // This file contains the version information for the application
 // It is automatically updated when a new release is created
 
-export const VERSION = '1.0.0'; // This will match the package.json version 
+export const VERSION = '1.0.1'; // Updated by GitHub Actions
