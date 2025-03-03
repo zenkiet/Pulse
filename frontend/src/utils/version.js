6a1a43df8637850689c33f633af0b86e23f502e7
courtmanr@gmail.com <courtmanr@gmail.com>
2025-03-03 15:01:26 +0000

    Update version to 1.0.15

--- frontend/src/utils/version.js
+++ frontend/src/utils/version.js
@@ -1,4 +1,4 @@
 // This file contains the version information for the application
 // It is automatically updated when a new release is created
 
-export const VERSION = '1.0.13'; // Updated by GitHub Actions
+export const VERSION = '1.0.15'; // Updated manually
