41a693d349d5e75182dd761b555c29710b3b709e
courtmanr@gmail.com <courtmanr@gmail.com>
2025-04-21 10:56:35 +0100

    fix(script): Add gpg dependency for NodeSource key import

--- scripts/install-pulse.sh
+++ scripts/install-pulse.sh
@@ -44,8 +44,8 @@ apt_update_upgrade() {
 }
 
 install_dependencies() {
-    print_info "Installing necessary dependencies (git, curl, sudo)..."
-    if apt-get install -y git curl sudo > /dev/null; then
+    print_info "Installing necessary dependencies (git, curl, sudo, gpg)..."
+    if apt-get install -y git curl sudo gpg > /dev/null; then
         print_success "Dependencies installed."
     else
         print_error "Failed to install dependencies."
