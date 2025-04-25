6ce1c86989c19807b41eb9450bdf9ecf26847ef2
courtmanr@gmail.com <courtmanr@gmail.com>
2025-04-25 22:40:19 +0100

    fix: Add safelist to tailwind config for dynamic classes

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     './src/public/**/*.html',
     './src/public/**/*.js',
   ],
+  safelist: [
+    'opacity-60',
+    {
+      pattern: /^(bg|text)-(red|yellow|green|blue)-(100|200|300|400|500|600|700|800|900)(\/50)?$/,
+    },
+    {
+      pattern: /^dark:(bg|text)-(red|yellow|green|blue)-(100|200|300|400|500|600|700|800|900)(\/50)?$/,
+    },
+    'vm-icon',
+    'ct-icon',
+  ],
   theme: {
     extend: {},
   },
